b69b39c90f50e53011268735dd553b5fd9607f80
Gnimuc <qiyupei@gmail.com>
2020-01-08 01:04:50 +0800

    Add a working naive OpenGL example

--- examples/App/src/App.jl
+++ examples/App/src/App.jl
@@ -1,16 +1,8 @@
 module App
 
-using Redux
-using CImGui
-
-include("../../todomvc/TodoMVC.jl")
-using .TodoMVC
-import .TodoMVC: Todo, TodoFilter
-
-include("Renderer.jl")
-using .Renderer
-
-include("../../todomvc/gui.jl")
+using GLFW
+using ModernGL
+using CSyntax
 
 Base.@ccallable function julia_main()::Cint
     try
@@ -23,8 +15,103 @@ Base.@ccallable function julia_main()::Cint
 end
 
 function real_main()
-    store = create_store(TodoMVC.todo_mvc, TodoMVC.INITIAL_STATE)
-    Renderer.render(()->todo_mvc_ui(store), width=600, height=300, title="App: TodoMVC")
+    # set up OpenGL context version
+    # it seems OpenGL 4.1 is the highest version supported by MacOS.
+    @static if Sys.isapple()
+        VERSION_MAJOR = 4
+        VERSION_MINOR = 1
+    end
+
+    @static if Sys.isapple()
+        GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, VERSION_MAJOR)
+        GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, VERSION_MINOR)
+        GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE)
+        GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    else
+        GLFW.DefaultWindowHints()
+    end
+
+    # create window
+    window = GLFW.CreateWindow(640, 480, "Hello Triangle")
+    @assert window != C_NULL "could not open window with GLFW3."
+    GLFW.MakeContextCurrent(window)
+
+    # get version info
+    renderer = unsafe_string(glGetString(GL_RENDERER))
+    version = unsafe_string(glGetString(GL_VERSION))
+    @info "Renderder: $renderer"
+    @info "OpenGL version supported: $version"
+
+    # enable depth test
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LESS)
+
+    # hard-coded shaders
+    vert_source = """
+    #version 410 core
+    in vec3 vp;
+    void main(void)
+    {
+        gl_Position = vec4(vp, 1.0);
+    }"""
+    frag_source = """
+    #version 410 core
+    out vec4 frag_colour;
+    void main(void)
+    {
+        frag_colour = vec4(0.5, 0.0, 0.5, 1.0);
+    }"""
+
+    # compile shaders
+    vert_shader = glCreateShader(GL_VERTEX_SHADER)
+    glShaderSource(vert_shader, 1, Ptr{GLchar}[pointer(vert_source)], C_NULL)
+    glCompileShader(vert_shader)
+    frag_shader = glCreateShader(GL_FRAGMENT_SHADER)
+    glShaderSource(frag_shader, 1, Ptr{GLchar}[pointer(frag_source)], C_NULL)
+    glCompileShader(frag_shader)
+
+    # create and link shader program
+    program = glCreateProgram()
+    glAttachShader(program, vert_shader)
+    glAttachShader(program, frag_shader)
+    glLinkProgram(program)
+
+    # vertex data
+    points = GLfloat[ 0.0,  0.5, 0.0,
+                      0.5, -0.5, 0.0,
+                     -0.5, -0.5, 0.0]
+
+    # create buffers located in the memory of graphic card
+    vbo = GLuint(0)
+    @c glGenBuffers(1, &vbo)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW)
+
+    # create VAO
+    vao = GLuint(0)
+    @c glGenVertexArrays(1, &vao)
+    glBindVertexArray(vao)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, C_NULL)
+    glEnableVertexAttribArray(0)
+
+    # set background color to gray
+    glClearColor(0.2, 0.2, 0.2, 1.0)
+
+    # render
+    while !GLFW.WindowShouldClose(window)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glUseProgram(program)
+        glBindVertexArray(vao)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        # check and call events
+        GLFW.PollEvents()
+        # swap the buffers
+        GLFW.SwapBuffers(window)
+    end
+
+    GLFW.DestroyWindow(window)
+
     return nothing
 end
 
